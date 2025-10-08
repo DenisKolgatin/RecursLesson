223f6b154da3d1e3fa597244e530997a5c9e2132
DenisKolgatin <denissova1453@gmail.com>
2025-10-08 03:47:00 +0300

    исправил

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Main {
 
+
     public static void main(String[] args) {
         compare(1);
         compare(2);
@@ -14,23 +15,18 @@ public class Main {
         System.out.println("=== Day " + day + " ===");
         int[] startNumbers = { 22, 3, 20, 25 };
         int iterative = chooseHobbyIterative(startNumbers, day);
-        int recursive = chooseHobbyRecursive(startNumbers, day, new int[day]);
+        int recursive = chooseHobbyRecursive(startNumbers, day);
         System.out.println("Iterative = " + iterative + " | Recursive = " + recursive);
         System.out.println();
     }
 
-    public static int chooseHobbyRecursive(int[] startNumbers, int day, int[] memory) {
+    public static int chooseHobbyRecursive(int[] startNumbers, int day) {
         if (day <= 0) {
             return startNumbers[3 + day];
         }
-        if (memory[day - 1] != 0) {
-            return memory[day - 1];
-        }
-        int prev = chooseHobbyRecursive(startNumbers, day - 1, memory);
-        int prePrePrev = chooseHobbyRecursive(startNumbers, day - 3, memory);
-        int result = (prev * prePrePrev % 10) + 1;
-        memory[day - 1] = result;
-        return result;
+        int prev = chooseHobbyRecursive(startNumbers, day - 1); // предыдущее значение
+        int prePrePrev = chooseHobbyRecursive(startNumbers, day - 3); // пре-пре-предыдущее значение
+        return (prev * prePrePrev) % 10 + 1;
     }
 
     public static int chooseHobbyIterative(int[] startNumbers, int day) {
@@ -42,9 +38,9 @@ public class Main {
         numbers.add(startNumbers[3]);
 
         for (int d = 0; d < day; d++) {
-            int index = d + 4;
-            int prev = numbers.get(index - 1);
-            int prePrePrev = numbers.get(index - 3);
+            int index = d + 4; // индексы дней в массиве сдвинуты на 4
+            int prev = numbers.get(index - 1); // предыдущее значение
+            int prePrePrev = numbers.get(index - 3); // пре-пре-предыдущее значение
             numbers.add((prev * prePrePrev) % 10 + 1);
         }
 
